f72d4d22a64c8f91b07975cefbfc6fc1606de71e
Roberto <r.realrangel@gmail.com>
2019-07-13 17:48:04 -0700

    Superupdate of months of work

--- lib/data_manager.py
+++ lib/data_manager.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """DROUGHT ASSESSMENT
 DESCRIPTION
@@ -10,9 +10,15 @@ AUTHOR
 LICENSE
     GNU General Public License
 """
-import numpy as np
+import sys
+
+from numpy import nan
+from pathlib2 import Path
+import gdal
+import ogr
 import pandas as pd
-import toml
+import toml as _toml
+import xarray as xr
 
 
 class Configurations():
@@ -20,24 +26,135 @@ class Configurations():
     """
     def __init__(self, config_file):
         self.config_file = config_file
-
-        with open(self.config_file, 'rb') as file_content:
-            config = toml.load(file_content)
+        config = _toml.load(config_file)
 
         for key, value in config.items():
             setattr(self, key, value)
 
 
-def read_input(input_file):
-    """Retrieve a time series from a .csv file.
+def list_files(parent_dir, ext):
+    """List all files in a directory with a specified extension.
 
-    PARAMETERS
-        input_file: string
-            The ful path of the input .csv file."""
-    return(pd.read_csv(
-            filepath_or_buffer=input_file,
-            index_col='time',
-            parse_dates=True))
+    Parameters
+        parent_dir: string
+            Full path of the directory of which the files are to be listed.
+        ext: string or list of strings
+            Extension(s) of the files to be listed.
+    """
+    parent_dir = Path(parent_dir)
+    files_list = []
+
+    if isinstance(ext, str):
+        patterns = ['**/*' + ext]
+
+    elif isinstance(ext, list):
+        patterns = ['**/*' + i for i in ext]
+
+    for patt in patterns:
+        files_list.extend(parent_dir.glob(pattern=patt))
+
+    return([str(i) for i in files_list])
+
+
+def vector2array(basin_vmap, resolution, nodata):
+    """
+    Parameters:
+        basin_vmap : string
+        resolution : float
+        nodata : float
+
+    Source:
+        https://bit.ly/2HxeOng
+    """
+    # Open the data source and read in the extent
+    source_ds = ogr.Open(basin_vmap)
+    source_layer = source_ds.GetLayer(0)
+    xmin, xmax, ymin, ymax = source_layer.GetExtent()
+
+    def round_mult(num, mult, to):
+        if to == 'up':
+            return(int(mult * round(float(num + mult) / mult)))
+
+        elif to == 'down':
+            return(int(mult * round(float(num - mult) / mult)))
+
+    xmin = round_mult(num=xmin, mult=resolution, to='down')
+    xmax = round_mult(num=xmax, mult=resolution, to='up')
+    ymin = round_mult(num=ymin, mult=resolution, to='down')
+    ymax = round_mult(num=ymax, mult=resolution, to='up')
+
+    # Create the destination data source
+    cols = int((xmax - xmin) / resolution)
+    rows = int((ymax - ymin) / resolution)
+    output_source = gdal.GetDriverByName('MEM').Create(
+        '', cols, rows, gdal.GDT_Byte
+        )
+    output_source.SetGeoTransform((xmin, resolution, 0, ymax, 0, -resolution))
+    output_band = output_source.GetRasterBand(1)
+    output_band.SetNoDataValue(nodata)
+
+    # Rasterize
+    gdal.RasterizeLayer(output_source, [1], source_layer, burn_values=[1])
+
+    # Read as array
+    return(output_band.ReadAsArray().astype(int))
+
+
+def clim_time_series(
+        input_dir, basin_vmap, resolution, nodata, missthresh, variable,
+        flowstate
+        ):
+    """Reads a datacube (t, x, y) and generates a time series.
+
+    Parameters:
+        input_dir : string
+        basin_vmap : string
+        resolution : float
+        nodata : float
+        missthresh : float
+        variable : string
+        flowstate : string
+
+    Outputs:
+    """
+    paths = list_files(
+        parent_dir=input_dir,
+        ext=['.nc', '.nc4']
+        )
+
+    data = xr.open_mfdataset(paths)
+    data = data.rename(name_dict={'__xarray_dataarray_variable__': variable})
+    mask = vector2array(basin_vmap, resolution, nodata)
+    inregion_cells = (mask == 1).sum()
+    min_cells = inregion_cells * missthresh
+    basin_area = inregion_cells * (resolution**2 / 1e6)
+
+    if flowstate == 'flow':
+        accum = (data[variable] * resolution**2).sum(['east', 'north'])
+        data_aggregated = accum / (basin_area * 1e6)
+
+    elif flowstate == 'state':
+        data_aggregated = data[variable].mean(['east', 'north'])
+
+    cells_per_date = data[variable].notnull().sum(['east', 'north'])
+
+    time_series = xr.where(
+        cells_per_date < min_cells, nan, data_aggregated
+        ).to_series()
+
+    return(time_series.reindex(sorted(time_series.index)))
+
+
+def sflo_time_series(input_file, basin_vmap, resolution, nodata):
+    data = xr.open_dataset(input_file)
+    data = data['disc_filled'].rename('observed')
+    data = data * (24 * 60 * 60) * 1000   # Convert m3/s to mm/d
+    mask = vector2array(basin_vmap, resolution, nodata)
+    inregion_cells = (mask == 1).sum()
+    basin_area = inregion_cells * (resolution**2 / 1e6)  # Prevent overflow.
+    time_series = (data / (basin_area * 1e6)).to_series()
+    time_series.index = time_series.index + pd.Timedelta(8, 'h')
+    return(time_series.reindex(sorted(time_series.index)))
 
 
 def scale_data(input_data, scale=10):
@@ -63,3 +180,65 @@ def scale_data(input_data, scale=10):
     """
     scaler = input_data.rolling(window=scale, center=True)
     return(scaler.mean())
+
+
+def esaccism_daily2annual_storing(
+        input_dir, output_dir, xmin, ymin, xmax, ymax
+        ):
+    daily_paths = list_files(
+        parent_dir=input_dir,
+        ext=['.nc', '.nc4']
+        )
+
+    for year in range(1978, 2019):
+        year_paths = [i for i in daily_paths if ('-' + str(year)) in i]
+
+        data = xr.open_mfdataset(
+            year_paths,
+            autoclose=True
+            )
+
+        clip_study_area = data.where(
+            (data.lat >= ymin) &
+            (data.lat <= ymax) &
+            (data.lon >= xmin) &
+            (data.lon <= xmax),
+            drop=True
+            )
+
+        output_name = output_dir + '/' + str(year) + '.nc4'
+        clip_study_area.to_netcdf(path=output_name)
+
+
+def progress_message(current, total, message="- Processing", units=None):
+    """Issue a messages of the progress of the process.
+
+    Generates a progress bar in terminal. It works within a for loop,
+    computing the progress percentage based on the current item
+    number and the total length of the sequence of item to iterate.
+
+    Parameters:
+        current : integer
+            The last item number computed within the for loop. This
+            could be obtained using enumerate() in when calling the for
+            loop.
+        total : integer
+            The total length of the sequence for which the for loop is
+            performing the iterations.
+        message : string (optional; default = "- Processing")
+    """
+    if units is not None:
+        progress = float(current)/total
+        sys.stdout.write("\r    {} ({:.1f} % of {} processed)".format(
+                message, progress * 100, units))
+
+    else:
+        progress = float(current)/total
+        sys.stdout.write("\r    {} ({:.1f} % processed)".format(
+                message, progress * 100))
+
+    if progress < 1:
+        sys.stdout.flush()
+
+    else:
+        sys.stdout.write('\n')
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """DROUGHT ASSESSMENT
 DESCRIPTION
@@ -24,25 +23,201 @@ REFERENCES
         Papers 23, (23), 25–25. https://doi.org/10.1016/0022-
         1694(69)90110-3
 """
+import os
+
 import lib.data_manager as dmgr
-from lib.threshold_level_method import DroughtIndicator
-import numpy as np
+import lib.plot_time_series as plt
+import lib.threshold_level_method as tlm
 
-config = dmgr.Configurations('config.toml')
-prec = dmgr.read_input(config.gral_prec_file)
-disc = dmgr.read_input(config.gral_disc_file)
-disc[prec.isnull().values] = np.nan
-prec[disc.isnull().values] = np.nan
+config = dmgr.Configurations(
+    '/home/realrangel/MEGA/projects/multiannual/04-doctorado/analysis'
+    '/02_drought_analysis/15014/config.toml'
+    )
 
-if config.pooling_method == 'ma':
-    prec = dmgr.scale_data(input_data=prec, scale=config.agg_scale)
-    disc = dmgr.scale_data(input_data=disc, scale=config.agg_scale)
+# TODO: Make a unique function for any variable.
+# Retrieve variables precipitation data.
+if config.vars['prec']['input_dir'] is not False:
+    prec_raw = dmgr.clim_time_series(
+        input_dir=config.vars['prec']['input_dir'],
+        basin_vmap=config.vars['basin_vmap'],
+        resolution=config.grid['res'],
+        nodata=config.grid['nodata'],
+        missthresh=config.grid['missthresh'],
+        variable='observed',
+        flowstate='flow'
+        )
 
-prec = DroughtIndicator(
-        indicator=prec,
-        pooling_method=config.pooling_method,
-        threshold=config.threshold)
-disc = DroughtIndicator(
-        indicator=disc,
-        pooling_method=config.pooling_method,
-        threshold=config.threshold)
+else:
+    prec_raw = None
+
+# Retrieve stream flow data.
+if config.vars['sflo']['input_file'] is not False:
+    sflo_raw = dmgr.sflo_time_series(
+        input_file=config.vars['sflo']['input_file'],
+        basin_vmap=config.vars['basin_vmap'],
+        resolution=config.grid['res'],
+        nodata=config.grid['nodata']
+        )
+
+else:
+    sflo_raw = None
+
+# Retrieve soil moisture data.
+if config.vars['smoi']['input_dir'] is not False:
+    smoi_raw = dmgr.smoi_time_series(
+        input_dir=config.vars['smoi']['input_dir'],
+        basin_vmap=config.vars['basin_vmap'],
+        resolution=config.grid['res'],
+        nodata=config.grid['nodata']
+        )
+
+else:
+    smoi_raw = None
+
+# Retrieve temperature data.
+if config.vars['tmea']['input_dir'] is not False:
+    tmea_raw = dmgr.clim_time_series(
+        input_dir=config.vars['tmea']['input_dir'],
+        basin_vmap=config.vars['basin_vmap'],
+        resolution=config.grid['res'],
+        nodata=config.grid['nodata'],
+        missthresh=config.grid['missthresh'],
+        variable='observed',
+        flowstate='state'
+        )
+
+else:
+    tmea_raw = None
+
+# Aggregate data.
+if prec_raw is not None:
+    prec = dmgr.scale_data(
+        input_data=prec_raw,
+        scale=config.vars['prec']['agg_scale']
+        )
+
+if sflo_raw is not None:
+    sflo = dmgr.scale_data(
+        input_data=sflo_raw,
+        scale=config.vars['sflo']['agg_scale']
+        )
+
+if smoi_raw is not None:
+    smoi = dmgr.scale_data(
+        input_data=smoi_raw,
+        scale=config.vars['smoi']['agg_scale']
+        )
+
+if tmea_raw is not None:
+    tmea = dmgr.scale_data(
+        input_data=tmea_raw,
+        scale=config.vars['tmea']['agg_scale']
+        )
+
+# Precipitation deficit
+precdef_run, precdef_onset, precdef_end = tlm.get_runs(
+    data=prec,
+    pooling_method=config.drought['pooling_method'],
+    detrend=config.vars['detrend'],
+    ref_level=config.drought['ref_level'],
+    show_positives=config.drought['show_positives'],
+    window=config.drought['ma_window']
+    )
+
+precdef_magnitude = tlm.runs_sum(runs=precdef_run)
+
+
+precdef_run_large = precdef_run[
+    precdef_magnitude <= precdef_magnitude.quantile(q=0.1)
+    ]
+
+precdef_peak = tlm.run_peak(
+    runs=precdef_run
+    )
+
+precdef_peak_large = tlm.run_peak(
+    runs=precdef_run_large
+    )
+
+precdef_onset_large = precdef_onset[
+    precdef_magnitude <= precdef_magnitude.quantile(q=0.1)
+    ]
+
+precdef_end_large = precdef_end[
+    precdef_magnitude <= precdef_magnitude.quantile(q=0.1)
+    ]
+
+precdef_magnitude_large = precdef_magnitude[
+    precdef_magnitude <= precdef_magnitude.quantile(q=0.1)
+    ]
+
+# Stream flow deficit
+sflodef_run, sflodef_onset, sflodef_end = tlm.get_runs(
+    data=sflo,
+    pooling_method=config.drought['pooling_method'],
+    detrend=config.vars['detrend'],
+    ref_level=config.drought['ref_level'],
+    show_positives=config.drought['show_positives'],
+    window=config.drought['ma_window']
+    )
+
+sflodef_magnitude = tlm.runs_sum(runs=sflodef_run)
+
+sflodef_run_large = sflodef_run[
+    sflodef_magnitude <= sflodef_magnitude.quantile(q=0.1)
+    ]
+
+sflodef_peak = tlm.run_peak(
+    runs=sflodef_run
+    )
+
+sflodef_peak_large = tlm.run_peak(
+    runs=sflodef_run_large
+    )
+
+sflodef_onset_large = sflodef_onset[
+    sflodef_magnitude <= sflodef_magnitude.quantile(q=0.1)
+    ]
+
+sflodef_end_large = sflodef_end[
+    sflodef_magnitude <= sflodef_magnitude.quantile(q=0.1)
+    ]
+
+sflodef_magnitude_large = sflodef_magnitude[
+    sflodef_magnitude <= sflodef_magnitude.quantile(q=0.1)
+    ]
+
+if config.plot['plot_time_series']:
+    if not os.path.isdir(config.plot['output_dir']):
+        os.mkdir(config.plot['output_dir'])
+
+    series1_ref = tlm.reference_value(data=prec)
+    series2_ref = tlm.reference_value(data=sflo)
+
+    first_year = max([
+        i.index.min()
+        for i in [prec_raw, sflo_raw, tmea_raw]
+        if i is not None
+        ]).year
+
+    last_year = min([
+        i.index.max()
+        for i in [prec_raw, sflo_raw, tmea_raw]
+        if i is not None
+        ]).year + 1
+
+    for date in range(first_year, last_year):
+        output_file = (
+            config.plot['output_dir'] + '/drought_' + str(date) + '-' +
+            str(date + 1)[-2:] + '.png'
+            )
+
+        plt.plot_ts(
+            series1=prec,
+            series2=sflo,
+            start_date=(str(date) + '-' + config.plot['plot_start']),
+            end_date=(str(date + 1) + '-' + config.plot['plot_end']),
+            output_file=output_file,
+            series1_ref=series1_ref,
+            series2_ref=series2_ref
+            )
