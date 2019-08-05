cdf34b74d9bca84acc1c55be1f428952f5beac67
Roberto Real-Rangel <rrealr@iingen.unam.mx>
2019-08-05 15:31:29 -0700

    Version was updated in the setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
      name='drought_t',
-     version='6.0.0',
+     version='0.3.1.1',
      description='Drought temporal analysis.',
      url='https://github.com/rrealrangel/drought_t',
      author='R. A. Real-Rangel',
